2ed0f975b5041d8138c65fc1cc8a8bc99a04ee13
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-04-29 21:49:51 -0400

    add styling to cart drawer

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -36,21 +36,25 @@ export const CartDrawer = ({ isOpen, onClose }: CartDrawerProps) => {
           {cartItems.length === 0 && <Text>It is empty in here</Text>}
           {cartItems.map((item) => (
             <div key={item.id}>
-              <HStack justifyContent={"space-between"}>
-                <Image src={item.product.image_url} alt={item.product.name} boxSize={"64px"} alignSelf={"center"} />
-                <Flex flexDir={"column"}>
-                  <div>
-                    <Text fontWeight={"bold"} fontSize="lg">
-                      {item.product.name}
-                    </Text>
-                    <Text>{item.product.primary_tag.toUpperCase()}</Text>
-                  </div>
+              <HStack justifyContent={"space-between"} py={2} borderBottom={"1px"}>
+                <Flex>
+                  <Image src={item.product.image_url} alt={item.product.name} boxSize={"64px"} alignSelf={"center"} />
+                  <Flex flexDir={"column"}>
+                    <div>
+                      <Text fontWeight={"bold"} fontSize="lg">
+                        {item.product.name}
+                      </Text>
+                      <Text>{item.product.primary_tag.toUpperCase()}</Text>
+                    </div>
+                  </Flex>
                 </Flex>
-                <Box>
-                  <Text textAlign={"center"}>Qty.</Text>
-                  <Text textAlign={"center"}>{item.quantity}</Text>
-                </Box>
-                <ActionButton onClick={() => removeFromCart(item.id)}>Remove</ActionButton>
+                <Flex alignItems={"center"}>
+                  <Box mr={6}>
+                    <Text textAlign={"center"}>Qty.</Text>
+                    <Text textAlign={"center"}>{item.quantity}</Text>
+                  </Box>
+                  <ActionButton onClick={() => removeFromCart(item.id)}>Remove</ActionButton>
+                </Flex>
               </HStack>
             </div>
           ))}
