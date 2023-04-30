23871554b1cda04a824e77064200591045453b02
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-04-29 23:25:33 -0400

    show item counts and total price

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -12,28 +12,36 @@ import {
   HStack,
   Image,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 
 import { ActionButton } from "~/components/Button";
 import { useCart } from "~/contexts/cart";
 
 // TODO: add quantity selector, and price calculation
+// TODO: should show selected product variant instead
 type CartDrawerProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 export const CartDrawer = ({ isOpen, onClose }: CartDrawerProps) => {
-  const { removeFromCart, cartItems } = useCart();
+  const { removeFromCart, cartItems, totalQuantity } = useCart();
 
   return (
     <Drawer isOpen={isOpen} placement="right" size={"md"} onClose={onClose}>
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
-        <DrawerHeader>Shopping Cart</DrawerHeader>
+        <DrawerHeader>Cart {totalQuantity && <span>({totalQuantity} items)</span>}</DrawerHeader>
 
         <DrawerBody>
-          {cartItems.length === 0 && <Text>It is empty in here</Text>}
+          {cartItems.length === 0 && (
+            <Flex height={"full"} alignItems={"center"} justifyContent={"center"}>
+              <Text fontWeight={"bold"} fontSize="lg">
+                Your cart is empty.
+              </Text>
+            </Flex>
+          )}
           {cartItems.map((item) => (
             <div key={item.id}>
               <HStack justifyContent={"space-between"} py={2} borderBottom={"1px"}>
@@ -61,9 +69,13 @@ export const CartDrawer = ({ isOpen, onClose }: CartDrawerProps) => {
         </DrawerBody>
 
         <DrawerFooter>
-          <ActionButton size={"lg"} w={"full"} as={Link} href={"/checkout"} onClick={() => undefined}>
-            Check out
-          </ActionButton>
+          <VStack w={"full"} alignItems={"flex-end"}>
+            <Text>Quantity: {totalQuantity}</Text>
+            <Text>Total: $0.00</Text>
+            <ActionButton size={"lg"} w={"full"} as={Link} href={"/checkout"} onClick={() => undefined}>
+              Checkout
+            </ActionButton>
+          </VStack>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
