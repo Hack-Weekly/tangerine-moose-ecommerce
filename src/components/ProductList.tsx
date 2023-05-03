aab3e443ba42e285c6d301f6ec355c8f6804fc7d
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-05-03 19:38:40 -0400

    fix styling on smaller screen

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,19 +1,17 @@
-import { Flex, Wrap, WrapItem } from "@chakra-ui/react";
+import { Flex, SimpleGrid } from "@chakra-ui/react";
 
 import Empty from "~/components/Empty";
 import ProductCard from "~/components/ProductCard";
-import type { Product } from "~/types/product";
+import { type Product } from "~/types/product";
 
 const ProductList = ({ productList }: { productList: Product[] }) => (
   <Flex justifyContent={"center"}>
     {productList && productList.length ? (
-      <Wrap justify="center">
+      <SimpleGrid alignContent={"center"} spacing={4} columns={[1, null, 2, 3]}>
         {productList.map((product) => (
-          <WrapItem key={product.id}>
-            <ProductCard product={product} />
-          </WrapItem>
+          <ProductCard key={product.id} product={product} />
         ))}
-      </Wrap>
+      </SimpleGrid>
     ) : (
       <Empty message={"sorry, we’re all out."} />
     )}
