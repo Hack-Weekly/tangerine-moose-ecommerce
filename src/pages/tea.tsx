ac35d5a6690c7cc33b3f4e5a3e7007cddd6a00a0
Hamzah <hamzahbmansour@gmail.com>
2023-05-04 13:00:55 -0700

    add more sample data, incl. a fixed-price/no-variant product

--- src/pages/tea.tsx
+++ src/pages/tea.tsx
@@ -6,7 +6,7 @@ import ProductList from "~/components/ProductList";
 import { teaProducts } from "~/data/data";
 
 const Tea: NextPage = () => {
-  const filters = ["loose leaf", "tea bags"];
+  const filters = ["loose leaf", "tea bags", "black tea", "green tea"];
   const [filter, setFilter] = useState<string[]>([]);
   const filteredResult = teaProducts.filter(({ tags }) => filter.every((filter) => tags.includes(filter)));
 
